55a6824bf22f163d882f829d78a897e1451d6357
arielrojas-github <6300323102@mcsdgas.com>
2024-12-18 18:57:52 -0500

    1

--- other/app.js
+++ other/app.js
@@ -130,7 +130,7 @@ const changeQuantityCart = (product_id, type) => {
 
 const initApp = () => {
     // get data product
-    fetch('other/products.json')
+    fetch('./products.json')
     .then(response => response.json())
     .then(data => {
         products = data;
